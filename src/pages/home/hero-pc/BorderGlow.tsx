fec3487a6dd4a5f6ef81eb9e28f0c0b13c5fe701
Luojie <494620815@qq.com>
2026-07-11 20:49:56 +0800

    chore: set border glow highlight to purple

--- src/pages/home/hero-pc/BorderGlow.tsx
+++ src/pages/home/hero-pc/BorderGlow.tsx
@@ -67,7 +67,7 @@ function animateValue({ start = 0, end = 100, duration = 1000, delay = 0, ease =
 }
 
 export default function BorderGlow({
-  children, className = "", edgeSensitivity = 30, glowColor = "40 80 80",
+  children, className = "", edgeSensitivity = 30, glowColor = "271 70 70",
   backgroundColor = "#120F17", borderRadius = 28, glowRadius = 40,
   glowIntensity = 1, coneSpread = 25, animated = false,
   colors = ["#c084fc", "#f472b6", "#38bdf8"], fillOpacity = 0.5,
